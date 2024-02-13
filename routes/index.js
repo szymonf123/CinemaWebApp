d90d276831fe0fde0ad317b0b163c5101784f9ff
szymonf123 <fortunaszymon@wp.pl>
2024-02-13 23:20:41 +0100

    "Add new routers"

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,36 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.render('index', {title : 'Kinomaniak', phone : '123 456 789', email : 'admin@kinomaniak.pl'});
 });
 
-router.get('/insert_movie', function(req, res, next) {
-  res.render('insert_movie', {title : 'Kinomaniak', phone : '123 456 789'});
+router.get('/admin', function(req, res) {
+  res.render('admin', {title : 'Kinomaniak'});
 });
 
-router.get('/admin', function(req, res, next) {
-  res.render('admin', {title : 'Kinomaniak', phone : '123 456 789'});
+router.get('/insert_movie', function(req, res) {
+  res.render('insert_movie', {title : 'Kinomaniak'});
+});
+
+router.get('/update_movie', function(req, res) {
+  res.render('update_movie', {title : 'Kinomaniak'});
+});
+
+router.get('/delete_movie', function(req, res) {
+  res.render('delete_movie', {title : 'Kinomaniak'});
+});
+
+router.get('/insert_seance', function(req, res) {
+  res.render('insert_seance', {title : 'Kinomaniak'});
+});
+
+router.get('/update_seance', function(req, res) {
+  res.render('update_seance', {title : 'Kinomaniak'});
+});
+
+router.get('/delete_seance', function(req, res) {
+  res.render('delete_seance', {title : 'Kinomaniak'});
 });
 
 module.exports = router;
